32d6a175cffa8fafb304dbd0d8063a55ea1d7824
Andrei <i.andrei.abramov@gmail.com>
2021-10-01 20:06:26 +0300

    Page for invalid url request created

---
+++ project/src/components/missing-page/missing-page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MissingPage() {
+  return (
+    <div>
+      <p>
+        Error 404. Requested page not found.
+      </p>
+    </div>
+  );
+}
+
+export default MissingPage;
