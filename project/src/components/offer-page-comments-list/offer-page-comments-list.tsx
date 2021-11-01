27f6ae4507fa437ff3a3465ddc03d1de7790d6c9
Andrei <i.andrei.abramov@gmail.com>
2021-11-01 14:19:31 +0300

    Max number of comments at the offer-page corrected

---
+++ project/src/components/offer-page-comments-list/offer-page-comments-list.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { CommentsFrontTypes } from '../../types/comments-types';
+import { getMillisecondsFromDate } from '../../utils/common-utils';
+import OfferPageComment from '../offer-page-comment/offer-page-comment';
+import { MAX_COMMENTS_TO_SHOW } from '../../const';
+
+export type OfferPageCommentsListTypes = {
+  commentsData: CommentsFrontTypes[],
+}
+
+function OfferPageCommentsList(props: OfferPageCommentsListTypes): JSX.Element {
+  const { commentsData } = props;
+  const commentsList = commentsData
+    .sort((a, b) => (
+      getMillisecondsFromDate(a.date) - getMillisecondsFromDate(b.date)
+    ))
+    .slice(-MAX_COMMENTS_TO_SHOW)
+    .map((dataItem) => (
+      <OfferPageComment
+        key={ dataItem.id }
+        commentData={ dataItem }
+      />
+    ));
+
+  return (
+    <>
+      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{ commentsData.length }</span></h2>
+      <ul className="reviews__list">
+        { commentsList }
+      </ul>
+    </>
+  );
+}
+
+export default OfferPageCommentsList;
