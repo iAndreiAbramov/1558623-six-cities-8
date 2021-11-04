5590b2b35e27710fe28caa66d20412dcade42775
Andrei <i.andrei.abramov@gmail.com>
2021-11-04 21:39:15 +0300

    Random city link to home page city added to login page

--- project/src/components/login-page/login-page.tsx
+++ project/src/components/login-page/login-page.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import LoginPageForm from '../login-page-form/login-page-form';
 import PageHeaderLogo from '../page-header-logo/page-header-logo';
+import { getRandomArrayItem } from '../../utils/common-utils';
+import { Cities } from '../../const';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { initActiveCityAction } from '../../store/api-actions';
 
 function LoginPage(): JSX.Element {
+  const randomCityName = getRandomArrayItem(Object.keys(Cities));
+  const dispatch = useDispatch();
   return (
     <div className="page page--gray page--login">
       <header className="header">
@@ -16,16 +23,17 @@ function LoginPage(): JSX.Element {
         <div className="page__login-container container">
           <section className="login">
             <h1 className="login__title">Sign in</h1>
-
             <LoginPageForm />
-
           </section>
           <section className="locations locations--login locations--current">
             <div className="locations__item">
-              {/*todo Разобраться куда ссылка и зменить на Link*/ }
-              <a className="locations__item-link" href="#">
-                <span>Amsterdam</span>
-              </a>
+              <Link className="locations__item-link" to="/">
+                <span
+                  onClick={ () => dispatch(initActiveCityAction(randomCityName)) }
+                >
+                  { randomCityName }
+                </span>
+              </Link>
             </div>
           </section>
         </div>
