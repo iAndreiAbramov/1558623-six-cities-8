aaeb4afffbc29c8f17f276f18d21b39ab8d1be61
Andrei <i.andrei.abramov@gmail.com>
2021-11-09 01:30:54 +0300

    Poor router test added

--- project/src/components/app/app.test.tsx
+++ project/src/components/app/app.test.tsx
@@ -1,30 +1,52 @@
 import React from 'react';
+import { Action, AnyAction, Store } from 'redux';
 import { configureMockStore } from '@jedmao/redux-mock-store';
 import { createMemoryHistory } from 'history';
 import { Provider } from 'react-redux';
-import { render, screen } from '@testing-library/react';
-import { Router } from 'react-router-dom';
-import App from './app';
-import { AppRoute, AuthorizationStatus, Cities, DEFAULT_CITY_NAME, FetchStatus } from '../../const';
-import { offersFrontMock, pointsMock } from '../../mocks/mock-offers';
+import { render } from '@testing-library/react';
+import { Route, Router, Switch } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus, FetchStatus } from '../../const';
+import PrivateRoute from '../private-route/private-route';
 
 const history = createMemoryHistory();
 const mockStore = configureMockStore();
 
-const store = mockStore({
+const storeWithAuth = mockStore({
   USER: { authorization: AuthorizationStatus.Auth },
   STATUS: { fetchStatus: FetchStatus.Success },
-  HOME: {
-    activeCity: Cities[DEFAULT_CITY_NAME],
-    offersData: offersFrontMock,
-    pointsForMap: pointsMock,
-  },
 });
 
-const fakeApp = (
+const storeWithNoAuth = mockStore({
+  USER: { authorization: AuthorizationStatus.Auth },
+  STATUS: { fetchStatus: FetchStatus.Success },
+});
+
+const getFakeApp = <A extends Action = AnyAction>(store: Store<any, A>) => (
   <Provider store={ store }>
     <Router history={ history }>
-      <App />
+      <Switch>
+        <Route path={ AppRoute.Home } exact>
+          <h1>Home page</h1>
+        </Route>
+
+        <Route path={ AppRoute.Login } exact>
+          <h1>Login page</h1>
+        </Route>
+
+        <PrivateRoute
+          exact
+          path={ AppRoute.Favorites }
+          render={ () => (<h1>Favorites page</h1>) }
+        />
+
+        <Route path={ AppRoute.OfferId } exact>
+          <h1>Offer page</h1>
+        </Route>
+
+        <Route>
+          <h1>Not found page</h1>
+        </Route>
+      </Switch>
     </Router>
   </Provider>
 );
@@ -32,11 +54,26 @@ const fakeApp = (
 describe('Component App:', () => {
   it('should render home page when user is on route "/"', () => {
     history.push(AppRoute.Home);
-    render(fakeApp);
+    const { getByText } = render(getFakeApp(storeWithAuth));
+    expect(getByText(/Home page/i)).toBeInTheDocument();
+  });
 
-    expect(screen.getByTestId('header-container')).toBeInTheDocument();
-    expect(screen.getByTestId('tabs-container')).toBeInTheDocument();
-    expect(screen.getByTestId('list-container')).toBeInTheDocument();
-    expect(screen.getByTestId('map-container')).toBeInTheDocument();
+  it('should render login page when user is on route "/login"', () => {
+    history.push(AppRoute.Login);
+    const { getByText } = render(getFakeApp(storeWithNoAuth));
+    expect(getByText(/Login page/i)).toBeInTheDocument();
+  });
+
+  it('should render offer page when user is on route "/offer/id"', () => {
+    const testOfferId = '1';
+    history.push(`${ AppRoute.Offer }/${ testOfferId }`);
+    const { getByText } = render(getFakeApp(storeWithAuth));
+    expect(getByText(/Offer page/i)).toBeInTheDocument();
+  });
+
+  it('should render favorites page when user is on route "/favorites"', () => {
+    history.push(`${ AppRoute.Favorites }`);
+    const { getByText } = render(getFakeApp(storeWithAuth));
+    expect(getByText(/Favorites page/i)).toBeInTheDocument();
   });
 });
--- project/src/components/page-header-logo/page-header-logo.test.tsx
+++ project/src/components/page-header-logo/page-header-logo.test.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { configureMockStore } from '@jedmao/redux-mock-store';
 import { createMemoryHistory } from 'history';
 import { Provider } from 'react-redux';
-import { render } from '@testing-library/react';
-import { Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import { Router, Route } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import { Cities, DEFAULT_CITY_NAME } from '../../const';
-import { configureMockStore } from '@jedmao/redux-mock-store';
 import PageHeaderLogo from './page-header-logo';
 
 const history = createMemoryHistory();
@@ -15,17 +16,31 @@ const store = mockStore({
   }
 });
 
+const headerLogo = (
+  <Provider store={ store }>
+    <Router history={ history }>
+      <Route path="/" exact>
+        <h1>This is main page</h1>
+      </Route>
+      <Route>
+        <PageHeaderLogo />
+      </Route>
+    </Router>
+  </Provider>);
+
 describe('Component PageHeaderLogo', () => {
   it('should render correctly', () => {
-    const { getByAltText, getByRole } = render(
-      <Provider store={ store }>
-        <Router history={ history }>
-          <PageHeaderLogo />
-        </Router>
-      </Provider>,
-    );
+    const { getByAltText, getByRole } = render(headerLogo);
 
     expect(getByAltText('6 cities logo')).toBeInTheDocument();
     expect(getByRole('link')).toBeInTheDocument();
   });
+
+  it('should redirect to main page on click', () => {
+    render(headerLogo);
+
+    expect(screen.getByText(/This is main page/i)).not.toBeInTheDocument();
+    userEvent.click(screen.getByRole('link'));
+    expect(screen.getByText(/This is main page/i)).toBeInTheDocument();
+  })
 });
