b6eca68dd25fba61f439d5841e893d58464f9a92
Andrei <i.andrei.abramov@gmail.com>
2021-11-11 23:35:56 +0300

    Selectors test added

--- project/src/components/offer-page-main/offer-page-main.test.tsx
+++ project/src/components/offer-page-main/offer-page-main.test.tsx
@@ -6,14 +6,11 @@ import { render, screen } from '@testing-library/react';
 import { renderHook } from '@testing-library/react-hooks';
 import { Router } from 'react-router-dom';
 import thunk from 'redux-thunk';
+import { AuxProps } from '../../store/selectors.test';
 import { createApi } from '../../services/api';
 import { getCurrentHotel } from '../../store/selectors';
 import { mockStoreWithAuth } from '../../mocks/mock-store';
 import OfferPageMain from './offer-page-main';
-
-interface AuxProps {
-  children: ReactChild | ReactChildren;
-}
 
 describe('Component: OfferPageMain', () => {
   const history = createMemoryHistory();
@@ -25,8 +22,7 @@ describe('Component: OfferPageMain', () => {
 
   it('should render correctly', () => {
     const wrapper = ({ children }: AuxProps ) => (
-      <Provider store={ store }>{ children }</Provider>
-    );
+      <Provider store={ store }>{ children }</Provider>);
 
     const { result } = renderHook(() => useSelector(getCurrentHotel), { wrapper });
 
